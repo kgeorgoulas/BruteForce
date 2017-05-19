1a2c04a746cdd323ff3d2700bee607c8731a41cb
Konstantinos Georgoulas <kosgeo21@gmail.com>
2017-05-19 21:02:05 +0100

    trial

---
+++ cmake-build-debug/CMakeFiles/BruteForce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BruteForce.dir/main.cpp.o"
+  "BruteForce.pdb"
+  "BruteForce"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BruteForce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
