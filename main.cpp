1d22c92a870bc99586affb5bb4c895ed97b3b0be
Konstantinos Georgoulas <kosgeo21@gmail.com>
2017-05-22 23:14:15 +0100

    initializing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+//Brute force search 10x10
 
-int main() {
-    std::cout << "Hello, World HAHAHA!" << std::endl;
-    return 0;
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int number = 100; //number of particles in the domain
+vector < vector <double> > coordinates(number, vector<double>(3));
+
+int main()
+{
+    int counter =0;
+    for(int i=0; i<sqrt(number); i++)
+    {
+        for(int j=0; j<sqrt(number); j++)
+        {
+            coordinates[counter][0]=i;
+            coordinates[counter][1]=j;
+            coordinates[counter][2]=counter;
+            //cout << "particle " << coordinates[counter][2] << " coordinates " << coordinates[counter][0] << " , " << coordinates[counter][1] << endl;
+            counter = counter + 1;
+        }
+    }
+
 }
